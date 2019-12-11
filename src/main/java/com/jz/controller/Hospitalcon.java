9eec6eb779c0b7dfd697183a82c1980dbc2309da
zzydan <zzydan@aliyun.com>
2019-12-11 11:39:47 +0800

    Default Changelist

--- src/main/java/com/jz/controller/Hospitalcon.java
+++ src/main/java/com/jz/controller/Hospitalcon.java
@@ -12,6 +12,6 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class Hospitalcon {
     @Autowired
-    private HospitalService service;
+    private HospitalService  service;
 
 }
