9c05d0214d4cbe13ffb82b476de603efc9602c3b
zzydan <zzydan@aliyun.com>
2019-12-12 13:46:08 +0800

    新闻

--- src/main/java/com/wx_hospital/mapper/SecHospitalNewsMapper.java
+++ src/main/java/com/wx_hospital/mapper/SecHospitalNewsMapper.java
@@ -1,5 +1,11 @@
 package com.wx_hospital.mapper;
 
+import com.wx_hospital.pojo.SecHospitalNews;
+
+import java.util.List;
+
 public interface SecHospitalNewsMapper {
+    //查询医院动态的新闻
+    List<SecHospitalNews> SelectNew();
 
 }
